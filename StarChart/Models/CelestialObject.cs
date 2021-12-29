3702e387879a144f563496ffa1430d8f7b1d3545
Bigya Dahal <bdahal@uwm.com>
2021-12-29 13:40:37 -0500

    Completed module

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -14,6 +14,8 @@ namespace StarChart.Models
         [Required]
         public string Name { get; set; }
         public int? OrbitedObjectId { get; set; }
+
+        [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
 
